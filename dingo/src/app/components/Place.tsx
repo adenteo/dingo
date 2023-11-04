e41881ba1b957fbd541aedace775c1d70f79d126
Aden Teo <88359484+adenteo@users.noreply.github.com>
2023-11-04 02:48:59 -0400

    Add react intersection observer feature
    Location in view now highlights and pans on map automatically while scrolling.

---
+++ dingo/src/app/components/Place.tsx
@@ -0,0 +1,86 @@
+import { Dispatch, SetStateAction, useEffect } from "react";
+import { useInView } from "react-intersection-observer";
+
+interface PlaceDetails {
+    place: any;
+    map: any;
+    setSelectedMarker: Dispatch<SetStateAction<any>>;
+    selectedMarker: any;
+}
+
+const Place = ({
+    place,
+    map,
+    setSelectedMarker,
+    selectedMarker,
+}: PlaceDetails) => {
+    const handlePlaceClicked = (place: any) => {
+        // window.open(
+        //     "https://www.google.com/maps/search/?api=1&query=<address>&query_place_id=" +
+        //         placeId
+        // );
+        const markerLatLng = {
+            lat: place["geometry"]["location"].lat(),
+            lng: place["geometry"]["location"].lng(),
+        };
+        setSelectedMarker(markerLatLng);
+        map.setZoom(15);
+        map.panTo(markerLatLng);
+    };
+
+    const { ref, inView } = useInView({
+        /* Optional options */
+        threshold: 1,
+    });
+
+    useEffect(() => {
+        if (inView) {
+            handlePlaceClicked(place);
+        }
+    }, [inView]);
+
+    const isSelectedPlace =
+        selectedMarker &&
+        selectedMarker.lat === place["geometry"]["location"].lat() &&
+        selectedMarker.lng === place["geometry"]["location"].lng();
+
+    return (
+        <div
+            ref={ref}
+            onClick={() => {
+                handlePlaceClicked(place);
+            }}
+            className={`bg-white text-black rounded w-[90vw] flex flex-col mb-10 ${isSelectedPlace && "border-green-700 border-t-8"}`}
+        >
+            <div className="w-auto h-[20vh]">
+                <img
+                    src={place.photos?.[0].getUrl()}
+                    className="w-full h-full rounded-tl rounded-tr"
+                ></img>
+            </div>
+            {/* <div>{inView ? "true" : "false"}</div> */}
+            <div className="mt-2">
+                <div className="flex justify-between mx-2">
+                    <div className="font-semibold truncate">{place.name}</div>
+                    {place.rating ? (
+                        <div className="flex">
+                            <div className="mx-2 w-6 h-6 rounded-full bg-green-500 font-bold flex items-center justify-center text-xs">
+                                {place.rating}
+                            </div>
+                            <div className="font-semibold text-xs flex items-center justify-center">
+                                {`(${place.user_ratings_total})`}
+                            </div>
+                        </div>
+                    ) : (
+                        <div className="text-xs flex items-center justify-center">
+                            No Reviews.
+                        </div>
+                    )}
+                </div>
+                <div className="mx-2 mb-2 text-xs truncate">{place.vicinity}</div>
+            </div>
+        </div>
+    );
+};
+
+export default Place;
--- dingo/src/app/components/Places.tsx
+++ dingo/src/app/components/Places.tsx
@@ -1,49 +1,22 @@
+import { Dispatch, SetStateAction, useEffect } from "react";
+import Place from "./Place";
+
 interface PlacesDetails {
     places: [];
+    map: any;
+    selectedMarker: any;
+    setSelectedMarker: Dispatch<SetStateAction<any>>;
 }
 
-const Places = ({ places }: PlacesDetails) => {
-    console.log(places);
+const Places = ({ places, map, setSelectedMarker, selectedMarker }: PlacesDetails) => {
+    const sortedPlaces = places.sort(
+        (a: any, b: any) => b.user_ratings_total - a.user_ratings_total
+    );
 
-    const openPlaceGmap = (placeId: string) => {
-        window.open("https://www.google.com/maps/place/?q=place_id:" + placeId);
-    };
     return (
-        <div>
-            {places.map((place: any, key) => (
-                <div
-                    onClick={() => {
-                        openPlaceGmap(place.place_id);
-                    }}
-                    className="p-3 m-3 bg-white text-black rounded w-[80vw] flex"
-                    key={key}
-                >
-                    <img
-                        src={place.icon}
-                        className="w-14 h-14 rounded my-auto"
-                    ></img>
-                    <div className="mx-2">
-                        <div className="font-semibold">{place.name}</div>
-                        {place.rating ? (
-                            <div className="relative w-8 h-8 rounded-full bg-green-500 font-bold flex items-center justify-center text-xs">
-                                {place.rating}
-                            </div>
-                        ) : (
-                            <div className="text-xs">No Reviews Yet.</div>
-                        )}
-                        {/* <div className="text-xs">{place.Cuisine}</div>
-           <div className="flex">
-             <div className="flex m-2">
-               <span className="bg-green-400 rounded-xl px-2 py-1 text-xs font-extrabold">+{place.ExtraDistance}</span>
-               <span className="text-xs py-1 ml-1 font-extrabold">km</span>
-             </div>
-             <div className="flex m-2">
-               <span className="bg-green-400 rounded-xl px-2 py-1 text-xs font-extrabold">+{place.ExtraTime}</span>
-               <span className="text-xs py-1 ml-1 font-extrabold">mins</span>
-             </div>
-           </div> */}
-                    </div>
-                </div>
+        <div className="overflow-auto mt-4">
+            {sortedPlaces.map((place: any, key) => (
+                <Place key={key} place={place} map={map} setSelectedMarker={setSelectedMarker} selectedMarker={selectedMarker}/>
             ))}
         </div>
     );
